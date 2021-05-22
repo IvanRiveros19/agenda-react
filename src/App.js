9b593beb8b1f45d366c155b401ec6fcb14dc9ade
Ivan Riveros <riverosgarciaivan@gmail.com>
2021-05-21 20:02:50 -0500

    Estructura para trabajar con Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css'
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+//redux
+import {Provider} from "react-redux";
+import store from "./redux/store";
+
 import Login from "./components/Login/Login";
 
 function App() {
-  return (
-    <div className="App">
-        <Login />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Provider store={store}>
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                </Switch>
+            </Provider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
